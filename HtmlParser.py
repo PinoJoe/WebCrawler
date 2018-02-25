9e33c6919489126b2a51e61a33731d2af538ccc6
Github0116 <1203965520@qq.com>
2018-02-25 17:38:25 +0800

    1

---
+++ HtmlParser.py
@@ -0,0 +1,20 @@
+'''
+Created on 2018Äê2ÔÂ24ÈÕ
+
+@author: Administrator
+'''
+#coding:utf-8
+import re
+import urlparse
+from bs4 import BeautifulSoup
+class HtmlParser(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
