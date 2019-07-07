7031e332442bc41543df7876ae8c2bc650ac9b91
root <root@localhost.localdomain>
2019-07-07 23:42:32 +0800

    new

--- img/__init__.py
+++ img/__init__.py
@@ -3,11 +3,12 @@ import urllib
 from lxml import etree
 import requests
 from time import perf_counter
+import sys
 def Schedule(blocknum, blocksize, totalsize):
     per = 100.0 * blocknum * blocksize / totalsize
     if per > 100 :
         per = 100
-    print('当前下载进度: %d' %per)
+    sys.stdout.write('当前下载进度: %d' %per + '%\r')
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:58.0) Gecko/20100101 Firefox/58.0'
 headers = {'User-Agent':user_agent}
 r = requests.get('http://www.ivsky.com/tupian/ziranfengguang/',headers=headers)
